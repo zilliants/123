c2afe50e0657fbbc89cb8fbd01ed7cf35a75e142
Sergey <zilliants@gmail.com>
2020-08-28 22:56:25 +0500

    Revert "Revert "Revert "Добавил text.html"""
    This reverts commit b45f553938b51ea539f28fa925a91938f4b94ba1.

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-def test():
-    print('TEST!')
-for i in range(10):
-    test()
+def high_and_low(numbers):
+    numbers = [int(i) for i in numbers.split()]
+    a = max(numbers)
+    b = min(numbers)
+    numbers = str(a)+' '+str(b)
+    return numbers
+
+
+print(high_and_low("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"))
